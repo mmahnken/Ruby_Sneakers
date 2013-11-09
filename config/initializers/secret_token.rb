0026573dc34dd8a5d8a441e11ebf6fe299e7e105
nyghtowl <warrick.melanie@gmail.com>
2013-11-08 20:04:16 -0800

    Initial setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubySneakers::Application.config.secret_key_base = 'aa5139f4b828dde8bb83fdec2b832c2b8dc81da72e7aad578c381be253aac66304953536de9c2a15b5b4e4e367e6ef96abf12daaaac5c9fba54ca39f4e11c24f'
